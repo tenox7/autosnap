52b2698578d1eb700ef6df398b15ebd04bd11457
tenox7 <as@tenoware.com>
2017-11-30 23:48:38 -0800

    initial

--- zrs.sh
+++ zrs.sh
@@ -2,6 +2,6 @@
 # ZFS Rolling Snapshot v1.0 Antoni Sawicki. This file is Public Domain.
 [ $# -ne 4 ] && { echo "usage: $0 <volume> <type> 0 <last>"; exit; }
 [ ${3} -lt ${4} ] && ${0} ${1} ${2} $((${3}+1)) ${4}
-[ ${3} -eq ${4} ] && echo zfs destroy "${1}@${2}${3}"
-[ ${3} -gt 0 ] && echo zfs rename "${1}@${2}$((${3}-1))" "${1}@${2}${3}"
-[ ${3} -eq 0 ] && echo zfs snapshot "${1}@${2}${3}"
+[ ${3} -eq ${4} ] && zfs destroy "${1}@${2}${3}"
+[ ${3} -gt 0 ] && zfs rename "${1}@${2}$((${3}-1))" "${1}@${2}${3}"
+[ ${3} -eq 0 ] && zfs snapshot "${1}@${2}${3}"
