613e2a7aa16c5194590a7b4112e73e1ff447ae26
Antoni Sawicki <tenox@google.com>
2022-07-09 04:56:56 -0700

    update zfs script

--- zfs-autosnap.sh
+++ zfs-autosnap.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-# ZFS Rolling Snapshot v1.1 by Antoni Sawicki. Apache 2.0.
-[ ${#} -lt 3 ] && { echo "usage: $0 <volume> <type> <last>"; exit; }
-[ ${4:-0} -lt ${3} ] && ${0} ${1} ${2} ${3} $((${4:-0}+1))
+# ZFS Auto Snapshot v1.1 written by Antoni Sawicki. License Apache 2.0.
+# Copyright (c) 2022 Google LLC. This is not an official Google product.
+[ ${#} -lt 3 ] && { echo "usage: $0 <volname> <type> <#max>"; exit; }
+[ ${4:-0} -lt ${3} ] && "${0}" "${1}" "${2}" "${3}" "$((${4:-0}+1))"
 [ ${4:-0} -eq ${3} ] && zfs destroy "${1}@${2}${4:-0}"
 [ ${4:-0} -gt 0 ] && zfs rename "${1}@${2}$((${4:-0}-1))" "${1}@${2}${4:-0}"
 [ ${4:-0} -eq 0 ] && zfs snapshot "${1}@${2}${4:-0}"
